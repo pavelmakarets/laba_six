cf53b8e8bf2504ce202208b0fbd9342e605eb132
pavelmakarets <makaretsp@mail.ru>
2016-09-30 18:21:12 +0300

    version 1.0.1

--- Mas.h
+++ Mas.h
@@ -4,8 +4,27 @@ using namespace std;
 class Mas
 {
 public:
-	Mas(int,int);
-	void show();
+	Mas(int n, int key = 1)
+	{
+		this->n = n;
+		this->a = new int[n];
+		if (key == 1)
+		{
+			cout << "¬ведите массив : ";
+			for (int i = 0; i < n; i++)
+			{
+				cin >> this->a[i];
+			}
+		}
+	}
+	void show()
+	{
+		for (int i = 0; i < this->n; i++)
+		{
+			cout << this->a[i] << " ";
+		}
+		cout << endl;
+	}
 	/*friend Mas operator * (Mas a, Mas b)
 	{
 		int max = a.n > b.n ? a.n : b.n;
@@ -22,7 +41,69 @@ public:
 		}
 		return temp;
 	}*/
-	~Mas();
+	Mas operator * (Mas x)
+	{
+		int max = this->n > x.n ? this->n : x.n;
+		Mas big = this->n > x.n ? *this : x;
+		Mas small = this->n > x.n ? x : *this;
+
+		Mas* temp = new Mas(max,0);
+		for (int i = 0; i < small.n; i++)
+		{
+			temp->a[i] = small.a[i] * big.a[i];
+		}
+		for (int i = small.n; i < big.n; i++)
+		{
+			temp->a[i] = big.a[i];
+		}
+		return *temp;
+	}
+	bool operator == (Mas x)
+	{
+		if (this->n == x.n)
+		{
+			for (int i = 0; i < x.n; i++)
+			{
+				if (this->a[i] == x.a[i]);
+				else return false;
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	operator int ()
+	{
+		return this->n;
+	}
+	int operator [] (int x)
+	{
+		for (int i = 0; i < this->n; i++)
+		{
+			if (this->a[i] == x)
+				return i;
+		} 
+		return 0;
+	}
+	bool operator <= (Mas x)
+	{
+		if (this->n <= x.n)
+		{
+			for (int i = 0; i < x.n; i++)
+			{
+				if (this->a[i] <= x.a[i]);
+				else return false;
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	~Mas() = default;
 private:
 	int* a;
 	int n;
--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
 #include "Mas.h"
 
+int size(int);
+
 void main()
 {
 	setlocale(LC_ALL,"rus");
-	Mas a(2,1);
-	a.show();
-	Mas b(4, 1);
-	b.show();
+	Mas a(5);
+	//a.show();
+	Mas b(2);
+	//b.show();
 
+	Mas x(0,0);
+	x = a * b ;
+	x.show();
 
+	//if (a == b) cout << "Массивы равны." << endl;
+	//else cout << "Массивы не равны." << endl;
+
+	//cout << size(a) << endl;
+
+	//cout << a[4] << endl;
+
+	//if (a <= b) cout << "Массив a меньше либо равен b." << endl;
 
 	system("pause");
+}
+
+int size(int x)
+{
+	return x;
 }
